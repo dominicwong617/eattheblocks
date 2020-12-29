61cb7a3756de464ee57cb32edc543111742800a0
Dominic Wong <dominic@neatcommerce.com>
2020-12-30 00:04:49 +0800

    Compound Dashboard - frontend

--- defi-development-mastery/project-compound-dashboard/apy.js
+++ defi-development-mastery/project-compound-dashboard/apy.js
@@ -11,12 +11,12 @@ const opf = Compound.util.getAddress(Compound.PriceFeed);
 const cTokenDecimals = 8;
 const blocksPerDay = 4 * 60 * 24;
 const daysPerYear = 365;
-const ethMaintissa = Math.pow(10, 18);
+const ethMantissa = Math.pow(10, 18);
 
 async function calculateSupplyApy(cToken) {
   const supplyRatePerBlock = await Compound.eth.read(
     cToken,
-    'function supplyRatePerBlock() returns(uint)',
+    'function supplyRatePerBlock() returns (uint)',
     [],
     { provider },
   );
@@ -24,38 +24,38 @@ async function calculateSupplyApy(cToken) {
   return 100 * (Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear - 1) - 1);
 }
 
-async function calculateCompApy(cToken, ticker underlyingDecimals) {
+async function calculateCompApy(cToken, ticker, underlyingDecimals) {
   let compSpeed = await Compound.eth.read(
     comptroller,
-    'function compSpeeds(address cToken) public view returns(uint)',
+    'function compSpeeds(address cToken) public view returns (uint)',
     [cToken],
     { provider },
   );
 
   let compPrice = await Compound.eth.read(
     opf,
-    'function price(string memory symbol) external view returns(uint)',
+    'function price(string memory symbol) external view returns (uint)',
     [Compound.COMP],
     { provider },
   );
 
   let underlyingPrice = await Compound.eth.read(
     opf,
-    'function price(string memory symbol) external view returns(uint)',
+    'function price(string memory symbol) external view returns (uint)',
     [ticker],
     { provider },
   );
 
   let totalSupply = await Compound.eth.read(
     cToken,
-    'function totalSupply() public view returns(uint)',
+    'function totalSupply() public view returns (uint)',
     [],
     { provider },
   );
 
   let exchangeRate = await Compound.eth.read(
     cToken,
-    'function exchangeRateCurrent() public returns(unit)',
+    'function exchangeRateCurrent() returns (uint)',
     [],
     { provider },
   );
@@ -64,7 +64,7 @@ async function calculateCompApy(cToken, ticker underlyingDecimals) {
   compPrice = compPrice / 1e6;
   underlyingPrice = underlyingPrice / 1e6;
   exchangeRate = +exchangeRate.toString() / ethMantissa;
-  totalSupply = +totalSupply().toString() * exchangeRate * underlyingPrice / Math.pow(10, underlyingDecimals);
+  totalSupply = +totalSupply.toString() * exchangeRate * underlyingPrice / Math.pow(10, underlyingDecimals);
   const compPerDay = compSpeed * blocksPerDay;
 
   return 100 * (compPrice * compPerDay / totalSupply) * daysPerYear;
