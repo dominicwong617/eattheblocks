096b43b4fb8eb0295e389dd0af2166bff71788ca
Dominic Wong <dominic@neatcommerce.com>
2020-12-30 00:06:22 +0800

    Compound Dashboard - fix a few things

--- defi-development-mastery/project-compound-dashboard/apy.js
+++ defi-development-mastery/project-compound-dashboard/apy.js
@@ -13,12 +13,12 @@ const blocksPerDay = 4 * 60 * 24;
 const daysPerYear = 365;
 const ethMantissa = Math.pow(10, 18);
 
-async function calculateSupplyApy(cToken) {
+async function calculateSupplyApy(cToken, ticker) {
   const supplyRatePerBlock = await Compound.eth.read(
     cToken,
     'function supplyRatePerBlock() returns (uint)',
     [],
-    { provider },
+    { provider }
   );
 
   return 100 * (Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear - 1) - 1);
@@ -27,58 +27,49 @@ async function calculateSupplyApy(cToken) {
 async function calculateCompApy(cToken, ticker, underlyingDecimals) {
   let compSpeed = await Compound.eth.read(
     comptroller,
-    'function compSpeeds(address cToken) public view returns (uint)',
-    [cToken],
-    { provider },
+    'function compSpeeds(address cToken) public returns (uint)',
+    [ cToken ],
+    { provider }
   );
 
   let compPrice = await Compound.eth.read(
     opf,
     'function price(string memory symbol) external view returns (uint)',
-    [Compound.COMP],
-    { provider },
-  );
-
-  let underlyingPrice = await Compound.eth.read(
-    opf,
-    'function price(string memory symbol) external view returns (uint)',
-    [ticker],
-    { provider },
+    [ Compound.COMP ],
+    { provider }
   );
 
   let totalSupply = await Compound.eth.read(
     cToken,
-    'function totalSupply() public view returns (uint)',
+    'function totalSupply() returns (uint)',
     [],
-    { provider },
+    { provider }
   );
 
   let exchangeRate = await Compound.eth.read(
     cToken,
     'function exchangeRateCurrent() returns (uint)',
     [],
-    { provider },
+    { provider }
   );
 
-  compSpeed = compSpeed / 1e18;
-  compPrice = compPrice / 1e6;
-  underlyingPrice = underlyingPrice / 1e6;
   exchangeRate = +exchangeRate.toString() / ethMantissa;
-  totalSupply = +totalSupply.toString() * exchangeRate * underlyingPrice / Math.pow(10, underlyingDecimals);
+  compSpeed = compSpeed / 1e18; // COMP has 18 decimal places
+  compPrice = compPrice / 1e6;  // price feed is USD price with 6 decimal places
+  totalSupply = (+totalSupply.toString() * exchangeRate) / (Math.pow(10, underlyingDecimals));
   const compPerDay = compSpeed * blocksPerDay;
 
-  return 100 * (compPrice * compPerDay / totalSupply) * daysPerYear;
+  return 100 * (compPrice * compPerDay / totalSupply) * 365;
 }
 
-async function calculateApy(cTokenTicker, underlyingTicker) {
-  const underlyingDecimals = Compound.decimals[cTokenTicker];
-  const cTokenAddress = Compound.util.getAddress(cTokenTicker);
+async function calculateApy(cToken, ticker) {
+  const underlyingDecimals = Compound.decimals[cToken.slice(1, 10)];
+  const cTokenAddress = Compound.util.getAddress(cToken);
   const [supplyApy, compApy] = await Promise.all([
-    calculateSupplyApy(cTokenAddress),
-    calculateCompApy(cTokenAddress, underlyingTicker, underlyingDecimals),
+    calculateSupplyApy(cTokenAddress, ticker),
+    calculateCompApy(cTokenAddress, ticker, underlyingDecimals)
   ]);
-
-  return { ticker: underlyingTicker, supplyApy, compApy };
+  return {ticker, supplyApy, compApy};
 }
 
 export default calculateApy;
